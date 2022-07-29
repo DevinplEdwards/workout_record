67729d39853ecc8d4bb0453e0d78ae29e5648455
devinple <devinpledwards@live.com>
2022-07-29 20:09:14 +0000

    generate 1st controller + scaffold

---
+++ app/views/excercises/_excercise.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! excercise, :id, :type, :created_at, :updated_at
+json.url excercise_url(excercise, format: :json)
---
+++ test/system/excercises_test.rb
@@ -0,0 +1,43 @@
+require "application_system_test_case"
+
+class ExcercisesTest < ApplicationSystemTestCase
+  setup do
+    @excercise = excercises(:one)
+  end
+
+  test "visiting the index" do
+    visit excercises_url
+    assert_selector "h1", text: "Excercises"
+  end
+
+  test "creating a Excercise" do
+    visit excercises_url
+    click_on "New Excercise"
+
+    fill_in "Type", with: @excercise.type
+    click_on "Create Excercise"
+
+    assert_text "Excercise was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Excercise" do
+    visit excercises_url
+    click_on "Edit", match: :first
+
+    fill_in "Type", with: @excercise.type
+    click_on "Update Excercise"
+
+    assert_text "Excercise was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Excercise" do
+    visit excercises_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Excercise was successfully destroyed"
+  end
+end
