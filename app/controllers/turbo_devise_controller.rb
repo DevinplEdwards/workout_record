25d0c4e29d2f426fa5c6664424019684d7206863
devinple <devinpledwards@live.com>
2022-08-02 05:21:26 +0000

    added missing responder to turbo_devise_controller

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -1,18 +1,18 @@
-class TurboController < ApplicationController
+class TurboDeviseController < ApplicationController
     class Responder < ActionController::Responder
-        def to_turbo_stream
+      def to_turbo_stream
         controller.render(options.merge(formats: :html))
-        rescue ActionView::MissingTemplate => error
+      rescue ActionView::MissingTemplate => error
         if get?
-            raise error
+          raise error
         elsif has_errors? && default_action
-            render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+          render rendering_options.merge(formats: :html, status: :unprocessable_entity)
         else
-            redirect_to navigation_location
+          redirect_to navigation_location
         end
+      end
     end
-end
-
+  
     self.responder = Responder
     respond_to :html, :turbo_stream
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,6 +13,7 @@ class TurboFailureApp < Devise::FailureApp
     else
       super
     end
+  end
 
 
   def skip_format?
@@ -33,11 +34,15 @@ Devise.setup do |config|
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   config.parent_controller = 'TurboDeviseController'
-  config.navigational_formats = [' */*', :html, :turbo_stream]
-  # ==> Warden configuration
+
+# ==> Navigation configuration
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
+  
+# ==> Warden configuration
   config.warden do |manager|
     manager.failure_app = TurboFailureApp
   end
+
 end
   
 
@@ -45,7 +50,7 @@ end
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  #config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -79,12 +84,12 @@ end
   # Configure which authentication keys should be case-insensitive.
   # These keys will be downcased upon creating or modifying a user and when used
   # to authenticate or find a user. Default is :email.
-  config.case_insensitive_keys = [:email]
+  #config.case_insensitive_keys = [:email]
 
   # Configure which authentication keys should have whitespace stripped.
   # These keys will have whitespace before and after removed upon creating or
   # modifying a user and when used to authenticate or find a user. Default is :email.
-  config.strip_whitespace_keys = [:email]
+  #config.strip_whitespace_keys = [:email]
 
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
@@ -118,7 +123,7 @@ end
   # Notice that if you are skipping storage for all authentication paths, you
   # may want to disable generating routes to Devise's sessions controller by
   # passing skip: :sessions to `devise_for` in your config/routes.rb
-  config.skip_session_storage = [:http_auth]
+  #config.skip_session_storage = [:http_auth]
 
   # By default, Devise cleans up the CSRF token on authentication to
   # avoid CSRF token fixation attacks. This means that, when using AJAX
@@ -144,7 +149,7 @@ end
   # a value less than 10 in other environments. Note that, for bcrypt (the default
   # algorithm), the cost increases exponentially with the number of stretches (e.g.
   # a value of 20 is already extremely slow: approx. 60 seconds for 1 calculation).
-  config.stretches = Rails.env.test? ? 1 : 12
+  #config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
   # config.pepper = '91d7e2881a5b83bbc1e4f1e088c3bc72ccab373333cf9a193f9a74f94ecc7624f1f81f2ec8e08a334ad7f83e6e192bde27c4e9339e4e8d51e9779c2ee8d49bb0'
@@ -178,7 +183,7 @@ end
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  #config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
@@ -188,7 +193,7 @@ end
   # config.remember_for = 2.weeks
 
   # Invalidates all the remember me tokens when the user signs out.
-  config.expire_all_remember_me_on_sign_out = true
+  #config.expire_all_remember_me_on_sign_out = true
 
   # If true, extends the user's remember period when remembered via cookie.
   # config.extend_remember_period = false
@@ -199,12 +204,12 @@ end
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  #config.password_length = 6..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
-  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+  #config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
@@ -245,7 +250,7 @@ end
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
   # change their passwords.
-  config.reset_password_within = 6.hours
+  #config.reset_password_within = 6.hours
 
   # When set to false, does not sign a user in automatically after their password is
   # reset. Defaults to true, so a user is signed in automatically after a reset.
@@ -287,7 +292,7 @@ end
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  #config.sign_out_via = :delete
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
@@ -329,4 +334,3 @@ end
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-end
